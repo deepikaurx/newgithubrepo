4118e65967bda7a0b001c522ead435ea853abe32
deepikaurx <17868uk@saeinstitute.edu>
2018-04-18 12:48:42 +0100

    commit

--- AccountManagerProject/src/AccountPackage/Service.java
+++ AccountManagerProject/src/AccountPackage/Service.java
@@ -5,9 +5,9 @@ import java.util.Map;
 
 public class Service {
 
-	static Account account = new Account("deepi", "sd", 654987);
+	private static Account account = new Account("deepi", "sd", 654987);
 
-	static HashMap services = new HashMap();
+	private static HashMap services = new HashMap();
 
 	public static void main(String[] args) {
 		add();
@@ -15,10 +15,8 @@ public class Service {
 	}
 
 	public static void add() {
-
 		services.put(account.getAccountNumber(), account);
 		System.out.println("The account " + account.getAccountNumber() + " has been stored in our library");
-
 	}
 
 	public static void remove() {
@@ -26,4 +24,21 @@ public class Service {
 		System.out.println("The account " + account.getAccountNumber() + " has been removed from our store");
 	}
 
+	public static Account getAccount() {
+		return account;
+	}
+
+	public static void setAccount(Account account) {
+		Service.account = account;
+	}
+
+	public static HashMap getServices() {
+		return services;
+	}
+
+	public static void setServices(HashMap services) {
+		Service.services = services;
+	}
+
+	
 }
